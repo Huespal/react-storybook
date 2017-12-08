9cbeea2ccd15d63b27b13c3d0d25f09ce2c2a194
Michaela Hercogova <michaela.hercogova@gmail.com>
2017-12-08 19:09:26 +0100

    removed all solutions

--- src/components/Buttons/SocialButton.js
+++ src/components/Buttons/SocialButton.js
@@ -1,9 +1,4 @@
 import React from 'react'
-// styled components have been imported for you already
-import styled from 'styled-components'
-
-// create styled-component called Button with styles of .btn-social and .btn-outline classes
-// you can find those classes in the stylesheet in public/css/main.css
 
 const SocialButton = ({ to, icon, target = "_blank" }) => (
   <a
