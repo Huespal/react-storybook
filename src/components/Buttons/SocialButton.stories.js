aba0de73bd91ab9b8be7f9a2c365370a47c81564
Sanlu <dani.sanlu@blackdata.systems>
2017-12-16 17:25:47 +0100

    [Add] SocialButton Story

--- src/components/Buttons/SocialButton.stories.js
+++ src/components/Buttons/SocialButton.stories.js
@@ -3,8 +3,13 @@ import React from 'react';
 import { storiesOf } from '@storybook/react'
 import { text, boolean, number } from '@storybook/addon-knobs'
 import { withInfo } from '@storybook/addon-info'
+import SocialButton from './SocialButton'
 
 import '../../../public/css/font-awesome.min.css'
 
 // create story of your SocialButton component
 // have a look in the DemoButton.stories.js file for an example
+
+storiesOf('SocialButton', module)
+    .add('with outline', () => <SocialButton outline to="https://github.com//leanjscom" icon="github" />)
+    .add('without outline', () => <SocialButton to="https://github.com//leanjscom" icon="github" />);
